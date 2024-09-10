03f97968f12ea2549879b7cc5f7951341b630dce
Karina Camargo <kacamargoaraujo@gmail.com>
2024-09-10 16:34:04 -0300

    Inclusão do restante do código para atender a solicitação do tutor e conclusão do exercício - Ebac

---
+++ src/Teste.java
@@ -0,0 +1,5 @@
+public class Teste {
+    public static void main(String[] args) {
+        UtilizandoTabela.usarTabela(UtilizandoTabela.MinhaEntidade.class);
+    }
+}
---
+++ src/UtilizandoTabela.java
@@ -0,0 +1,17 @@
+
+public class UtilizandoTabela {
+
+    public static void usarTabela(Class<?> clazz) {
+        if (clazz.isAnnotationPresent(Tabela.class)) {
+            Tabela tabela = clazz.getAnnotation(Tabela.class);
+            System.out.println("Nome da Tabela: " + tabela.valor());
+        } else {
+            System.out.println("A classe " + clazz.getSimpleName() + " não está anotada com @Tabela.");
+        }
+    }
+
+    @Tabela(valor = "Teste_aula_ANNOTATION_EBAC")
+    public class MinhaEntidade {
+    }
+
+}
